0c639ec13905b8568d350c5d1f2a2171f1773f1b
rseto88 <rseto@it-rpub9.eastern.ewu.edu>
2014-02-23 17:55:24 -0800

    version 1

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,16 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+	new Maplace({
+	show_markers: true,
+	locations: [{
+		lat: 47.659381,
+		lon: -117.4236639,
+		zoom: 17
+				}]
+			}).Load(); 
+
+
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
